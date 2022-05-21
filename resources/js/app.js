8df8c6cfdea486c54083c51e0103caa6b3117ef2
Roman <roma20.94.a@gmail.com>
2022-05-21 18:09:01 +0300

    install dropzone && create Index component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Index from "./components";
+import Index from "./components/Index";
 
 require('./bootstrap');
 
--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-      Index
+  <div class="w-25">
+      <div ref="dropzone" class="p-5 d-block bg-dark text-center text-light btn">
+          Upload
+      </div>
   </div>
 </template>
 
 <script>
+import Dropzone from 'dropzone'
 export default {
-    name: "Index"
+    name: "Index",
+
+
+    data() {
+        return {
+            dropzone: null,
+        }
+    },
+
+
+    mounted() {
+        this.dropzone = new Dropzone(this.$refs.dropzone, {
+            url: "/file/post",
+        })
+    },
+
 }
 </script>
 
